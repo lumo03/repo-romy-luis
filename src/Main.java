0e89e5ac6f8a58b41203328a0a53c156a53b33e6
Luis Moncada <luis.internet@icloud.com>
2022-09-02 11:54:03 +0200

    / ‘bin/Main.class’ / ‘src/Main.java’

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		var greeting = "Hello World";
 		System.out.println(greeting);
+		var randomNum = Main.generateRandomNumber();
+		System.out.println(randomNum);
 	}
 	
-	public int generateRandomNumber() {
+	public static int generateRandomNumber() {
 		return (int) Math.random() * 1000;
 	}
 	
