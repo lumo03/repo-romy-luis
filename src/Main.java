ceb783a6b916122c710029fba6c1141bcdf8a734
Luis Moncada <luis.internet@icloud.com>
2022-09-02 11:55:18 +0200

    / ‘bin/Main.class’ / ‘src/Main.java’

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,15 @@ public class Main {
 		System.out.println(greeting);
 		var randomNum = Main.generateRandomNumber();
 		System.out.println(randomNum);
+		Main.sayHello("Luis");
 	}
 	
 	public static int generateRandomNumber() {
 		return (int) Math.random() * 1000;
+	}
+	
+	public static void sayHello(String name) {
+		System.out.println(String.format("Hello %s!", name));
 	}
 	
 }
