0f6ce7fa2c5770905f812c594c7a40f44343f0be
Luis Moncada <luis.internet@icloud.com>
2022-09-04 23:05:57 +0200

    / ‘src/utils/JSONParser.java’

--- src/utils/JSONParser.java
+++ src/utils/JSONParser.java
@@ -3,7 +3,7 @@ package utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSONParser {
-	ObjectMapper objM
+	private ObjectMapper objMapper;
 	public JSONParser() {
 		 = new ObjectMapper();
 	}
