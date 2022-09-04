1aee840d74e0444c7f4f74219b05891fd62e47a8
Luis Moncada <luis.internet@icloud.com>
2022-09-04 22:00:06 +0200

    / ‘src/trees/GuessAnimal.java’

--- src/trees/GuessAnimal.java
+++ src/trees/GuessAnimal.java
@@ -3,67 +3,63 @@ package trees;
 import java.util.Scanner;
 
 public class GuessAnimal {
-    Scanner input = new Scanner(System.in);
-    Question root;
+	Scanner input = new Scanner(System.in);
+	Question root;
 
-    public GuessAnimal() {
-        root = new Question("Ist es ein Säugetier?");
-        root.yes = new Question("Löwe");
-        root.no =new Question( "Papagei");
-    }
+	public GuessAnimal() {
+		root = new Question("Ist es ein Säugetier?");
+		root.yes = new Question("Löwe");
+		root.no = new Question("Papagei");
+	}
 
-    public void play() {
-        System.out.println("Willkommen beim Expertensystem");
-        Question currentQuestion = root;
-        
-         while(true) {
-	        System.out.println(currentQuestion.content);
-	        Answer res = askForInput();
-	        System.out.println("Du hast die Antwort \""+ res.getResponseValue() +"\" ausgewählt.");
-	        currentQuestion = res == Answer.YES ? currentQuestion.yes : currentQuestion.no;
-        }
-    }
-    
-    public Answer askForInput() {
-    	String response = "";
-    	
-    	do {
-    		System.out.println("Gültige Antworten sind nur \"ja\" oder \"nein\"!");
-    		response = input.nextLine();
-    	} while(
-    			!response.toLowerCase().equals("ja") && 
-    			!response.toLowerCase().equals("nein")
-    	);
-    	
-    	return response.equals("ja") ? Answer.YES : Answer.NO;
-    }
+	public void play() {
+		System.out.println("Willkommen beim Expertensystem");
+		Question currentQuestion = root;
 
-    public static void main(String[] args) {
-        GuessAnimal tr = new GuessAnimal();
-        tr.play();
-    }
+		while (true) {
+			System.out.println(currentQuestion.content);
+			Answer res = askForInput();
+			System.out.println("Du hast die Antwort \"" + res.getResponseValue() + "\" ausgewählt.");
+			currentQuestion = res == Answer.YES ? currentQuestion.yes : currentQuestion.no;
+		}
+	}
+
+	public Answer askForInput() {
+		String response = "";
+
+		do {
+			System.out.println("Gültige Antworten sind nur \"ja\" oder \"nein\"!");
+			response = input.nextLine();
+		} while (!response.toLowerCase().equals("ja") && !response.toLowerCase().equals("nein"));
+
+		return response.equals("ja") ? Answer.YES : Answer.NO;
+	}
+
+	public static void main(String[] args) {
+		GuessAnimal tr = new GuessAnimal();
+		tr.play();
+	}
 }
 
 enum Answer {
-	YES("ja"),
-	NO("nein");
-	
+	YES("ja"), NO("nein");
+
 	String resValue;
-	
+
 	Answer(String resValue) {
 		this.resValue = resValue;
 	}
-	
+
 	public String getResponseValue() {
 		return resValue;
 	}
 }
 
 class Question {
-    String content;
-    Question yes, no;
+	String content;
+	Question yes, no;
 
-    public Question(String content) {
-        this.content = content;
-    }
+	public Question(String content) {
+		this.content = content;
+	}
 }
