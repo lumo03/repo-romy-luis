40baab7e456c839bd1a620666ec8914d41f29cff
Luis Moncada <luis.internet@icloud.com>
2022-09-04 22:47:09 +0200

    / ‘src/utils/FileReader.java’

--- src/utils/FileReader.java
+++ src/utils/FileReader.java
@@ -5,9 +5,16 @@ import java.io.File;
 public class FileReader {
 	private File file;
 	
+	public FileReader() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
 	public File getFile() {
 		return file;
 	}
 	
-	
+	public void setFile(File file) {
+		this.file = file;
+	}
 }
